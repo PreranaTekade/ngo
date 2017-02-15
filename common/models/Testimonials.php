640f53beec6a16275612db84595befdfc642668e
prerana <preranatekade25@gmail.com>
2017-02-15 18:20:41 +0530

    initial commit

---
+++ common/models/Testimonials.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "testimonials".
+ *
+ * @property integer $id
+ * @property string $testimonial
+ * @property string $name
+ * @property string $created_at
+ * @property integer $created_by
+ * @property string $updated_at
+ * @property integer $updated_by
+ * @property integer $is_deleted
+ */
+class Testimonials extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'testimonials';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['testimonial', 'name'], 'required'],
+            [['testimonial'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['created_by', 'updated_by', 'is_deleted'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'testimonial' => 'Testimonial',
+            'name' => 'Name',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+            'is_deleted' => 'Is Deleted',
+        ];
+    }
+}
